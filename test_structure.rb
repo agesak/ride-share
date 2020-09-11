1259d9984e3be9cd9d0cb13bf9dfd5cca0ab5768
Kareha Agesa <kmaagesa@gmail.com>
2020-09-10 21:40:59 -0700

    fully functioning

--- test_structure.rb
+++ test_structure.rb
@@ -1,6 +1,4 @@
-# max earner/highest avg rating should handle ties? are there ties?
-# don't use any enumerable methods? (only each_key)
-
+# 09.10.2020
 
 def summarize_driver(nested_hash, driver, value_index)
 	# summarize a given parameter for a given driver
@@ -8,84 +6,63 @@ def summarize_driver(nested_hash, driver, value_index)
 	# in the ride-specific array per driver
 	summary_list = []
 	nested_hash[:"#{driver}"].each do |ride|
-		# not sure why -1 unnests nested array here.. 
-		value = ride.values[-1][value_index]
+		value = ride.values[0][value_index]
 		summary_list.push(value)
 	end
 	return summary_list
 end
 
-# not fully filled out, just testing
-info = { 
-	DR0001: [ 
-	{ ride_1: ["3rd Feb 2016", 10, "RD0003", 3] },
-	{ ride_2: ["3rd Feb 2016", 30, "RD0015", 4] }
-			],
-
-	DR0002: [
-	{ ride_1: ["3rd Feb 2016", 25, "RD0073", 5] },
-	{ ride_2: ["4th Feb 2016", 15, "RD0013", 1] }
-]
+# big ole hash with rider info
+driver_hash = { 
+	DR0001: [
+      {ride_1: ["3rd Feb 2016", 10, "RD0003", 3]},
+      {ride_2: ["3rd Feb 2016", 30, "RD0015", 4]},
+      {ride_3: ["5th Feb 2016", 45, "RD0003", 2]}
+  ],
+  DR0002: [
+      {ride_1: ["3rd Feb 2016", 25, "RD0073", 5]},
+      {ride_2: ["4th Feb 2016", 15, "RD0013", 1]},
+      {ride_3: ["5th Feb 2016", 35, "RD0066", 3]}
+  ],
+	DR0003: [
+      {ride_1: ["4th Feb 2016", 5, "RD0066", 5]},
+      {ride_2: ["5th Feb 2016", 50, "RD0003", 2]}
+  ],
+	DR0004: [
+      {ride_1: ["3rd Feb 2016", 5, "RD0022", 5]},
+      {ride_2: ["4th Feb 2016", 10, "RD0022", 4]},
+      {ride_3: ["5th Feb 2016", 20, "RD0073", 5]}
+  ]
 }
 
 
 # riders per driver
-info.each_key do |key|
-	rides = info[:"#{key}"].length
+driver_hash.each_key do |key|
+	rides = driver_hash[:"#{key}"].length
 	puts "Driver #{key} has #{rides} rides"
 end
 
-
 # total amount of money each driver made
 max_earnings = {}
-info.each_key do |key|
-	costs = summarize_driver(info, key, 1)
+driver_hash.each_key do |key|
+	costs = summarize_driver(driver_hash, key, 1)
 	max_earnings[:"#{key}"] = costs.sum
 end
-
 max_earnings.each { |key, value| puts "Driver #{key} made $#{value}"}
-
-# without a method
-# info.each_key do |key|
-# 	# puts key
-# 	costs = []
-# 	info[:"#{key}"].each do |ride|
-# 		cost = ride.values[-1][1]
-# 		# puts cost
-# 		costs.push(cost)
-# 	end
-# 	puts "Driver #{key} made $#{costs.sum}"
-# end
 
 # average rating per driver
 avg_ratings = {}
-info.each_key do |key|
-	ratings = summarize_driver(info, key, -1)
+driver_hash.each_key do |key|
+	ratings = summarize_driver(driver_hash, key, -1)
 	avg_ratings[:"#{key}"] = ratings.sum/ratings.length.to_f
 end
-avg_ratings.each { |key, value| puts "Driver #{key} had an average rating of #{value}"}
-
-
-# without a method
-# info.each_key do |key|
-# 	# puts key
-# 	ratings = []
-# 	info[:"#{key}"].each do |ride|
-# 		rating = ride.values[-1][-1]
-# 		# puts rating
-# 		ratings.push(rating)
-# 	end
-# 	avg = ratings.sum/ratings.length.to_f
-# 	puts "Driver #{key} had an average rating of #{avg}"
-# end
+avg_ratings.each { |key, value| puts "Driver #{key} had an average rating of #{value.round(1)}"}
 
 # which driver made the most money?
-# this should handle ties...
 max_earner = max_earnings.max_by{|k,v| v}[0].to_s
 puts "Driver #{max_earner} made the most money"
 
 # which driver had highest average rating
-# this should handle ties...
 max_avg = avg_ratings.max_by{|k,v| v}[0].to_s
 puts "Driver #{max_avg} had the highest average rating"
 
--- worksheet.rb
+++ worksheet.rb
@@ -22,6 +22,27 @@
 # You should be copying and pasting the literal data
 # into this data structure, such as "DR0004"
 # and "3rd Feb 2016" and "RD0022"
+driver_hash = {
+    DR0001: [
+        {ride_1: ["3rd Feb 2016", 10, "RD0003", 3]},
+        {ride_2: ["3rd Feb 2016", 30, "RD0015", 4]},
+        {ride_3: ["5th Feb 2016", 45, "RD0003", 2]}
+    ],
+    DR0002: [
+        {ride_1: ["3rd Feb 2016", 25, "RD0073", 5]},
+        {ride_2: ["4th Feb 2016", 15, "RD0013", 1]},
+        {ride_3: ["5th Feb 2016", 35, "RD0066", 3]}
+    ],
+    DR0003: [
+        {ride_1: ["4th Feb 2016", 5, "RD0066", 5]},
+        {ride_2: ["5th Feb 2016", 50, "RD0003", 2]}
+    ],
+    DR0004: [
+        {ride_1: ["3rd Feb 2016", 5, "RD0022", 5]},
+        {ride_2: ["4th Feb 2016", 10, "RD0022", 4]},
+        {ride_3: ["5th Feb 2016", 20, "RD0073", 5]}
+    ]
+}
 
 ########################################################
 # Step 4: Total Driver's Earnings and Number of Rides
@@ -32,3 +53,45 @@
 # - the average rating for each driver
 # - Which driver made the most money?
 # - Which driver has the highest average rating?
+
+def summarize_driver(nested_hash, driver, value_index)
+  # summarize a given parameter for a given driver
+  # value index is the index of the item of interest
+  # in the ride-specific array per driver
+  summary_list = []
+  nested_hash[:"#{driver}"].each do |ride|
+    value = ride.values[0][value_index]
+    summary_list.push(value)
+  end
+  return summary_list
+end
+
+# the number of rides each driver has given
+driver_hash.each_key do |key|
+  rides = driver_hash[:"#{key}"].length
+  puts "Driver #{key} has #{rides} rides"
+end
+
+# the total amount of money each driver has made
+max_earnings = {}
+driver_hash.each_key do |key|
+  costs = summarize_driver(driver_hash, key, 1)
+  max_earnings[:"#{key}"] = costs.sum
+end
+max_earnings.each { |key, value| puts "Driver #{key} made $#{value}"}
+
+# the average rating for each driver
+avg_ratings = {}
+driver_hash.each_key do |key|
+  ratings = summarize_driver(driver_hash, key, -1)
+  avg_ratings[:"#{key}"] = ratings.sum/ratings.length.to_f
+end
+avg_ratings.each { |key, value| puts "Driver #{key} had an average rating of #{value.round(1)}"}
+
+# Which driver made the most money?
+max_earner = max_earnings.max_by{|k,v| v}[0].to_s
+puts "Driver #{max_earner} made the most money"
+
+# Which driver has the highest average rating?
+max_avg = avg_ratings.max_by{|k,v| v}[0].to_s
+puts "Driver #{max_avg} had the highest average rating"
